e1331b98f8b01dd762dbd07565186abb1503af5f
Saurabh Gupta <sggupta548@gmail.com>
2026-03-25 16:13:30 +0530

    full entity added

--- src/main/java/com/filmrental/FilmRental/model/Film.java
+++ src/main/java/com/filmrental/FilmRental/model/Film.java
@@ -1,6 +1,7 @@
 package com.filmrental.FilmRental.model;
 
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "film")
@@ -11,66 +12,164 @@ public class Film {
     private Long filmId;
 
     private String title;
+
     private String description;
+
+    @Column(name = "release_year")
+    private Integer releaseYear;
+
+    @Column(name = "language_id")
+    private Integer languageId;
+
+    @Column(name = "original_language_id")
+    private Integer originalLanguageId;
+
+    @Column(name = "rental_duration")
+    private Integer rentalDuration;
 
     @Column(name = "rental_rate")
     private Double rentalRate;
 
     private Integer length;
 
-	public Long getFilmId() {
-		return filmId;
-	}
+    @Column(name = "replacement_cost")
+    private Double replacementCost;
 
-	public void setFilmId(Long filmId) {
-		this.filmId = filmId;
-	}
+    private String rating;
 
-	public String getTitle() {
-		return title;
-	}
+    @Column(name = "special_features")
+    private String specialFeatures;
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
 
-	public String getDescription() {
-		return description;
-	}
+    // 🔹 Default Constructor
+    public Film() {
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    // 🔹 Parameterized Constructor
+    public Film(Long filmId, String title, String description, Integer releaseYear,
+                Integer languageId, Integer originalLanguageId, Integer rentalDuration,
+                Double rentalRate, Integer length, Double replacementCost,
+                String rating, String specialFeatures, LocalDateTime lastUpdate) {
+        this.filmId = filmId;
+        this.title = title;
+        this.description = description;
+        this.releaseYear = releaseYear;
+        this.languageId = languageId;
+        this.originalLanguageId = originalLanguageId;
+        this.rentalDuration = rentalDuration;
+        this.rentalRate = rentalRate;
+        this.length = length;
+        this.replacementCost = replacementCost;
+        this.rating = rating;
+        this.specialFeatures = specialFeatures;
+        this.lastUpdate = lastUpdate;
+    }
 
-	public Double getRentalRate() {
-		return rentalRate;
-	}
+    // 🔹 Getters and Setters
 
-	public void setRentalRate(Double rentalRate) {
-		this.rentalRate = rentalRate;
-	}
+    public Long getFilmId() {
+        return filmId;
+    }
 
-	public Integer getLength() {
-		return length;
-	}
+    public void setFilmId(Long filmId) {
+        this.filmId = filmId;
+    }
 
-	public void setLength(Integer length) {
-		this.length = length;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public Film(Long filmId, String title, String description, Double rentalRate, Integer length) {
-		super();
-		this.filmId = filmId;
-		this.title = title;
-		this.description = description;
-		this.rentalRate = rentalRate;
-		this.length = length;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public Film() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    public String getDescription() {
+        return description;
+    }
 
-    // Getters & Setters
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public Integer getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(Integer languageId) {
+        this.languageId = languageId;
+    }
+
+    public Integer getOriginalLanguageId() {
+        return originalLanguageId;
+    }
+
+    public void setOriginalLanguageId(Integer originalLanguageId) {
+        this.originalLanguageId = originalLanguageId;
+    }
+
+    public Integer getRentalDuration() {
+        return rentalDuration;
+    }
+
+    public void setRentalDuration(Integer rentalDuration) {
+        this.rentalDuration = rentalDuration;
+    }
+
+    public Double getRentalRate() {
+        return rentalRate;
+    }
+
+    public void setRentalRate(Double rentalRate) {
+        this.rentalRate = rentalRate;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    public Double getReplacementCost() {
+        return replacementCost;
+    }
+
+    public void setReplacementCost(Double replacementCost) {
+        this.replacementCost = replacementCost;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getSpecialFeatures() {
+        return specialFeatures;
+    }
+
+    public void setSpecialFeatures(String specialFeatures) {
+        this.specialFeatures = specialFeatures;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
 }
