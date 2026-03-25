13e8fe4b58f2c7e2f313fd8423e570fafd13d388
Saurabh Gupta <sggupta548@gmail.com>
2026-03-25 12:44:05 +0530

    start project

---
+++ src/main/java/com/filmrental/FilmRental/FilmRentalApplication.java
@@ -0,0 +1,13 @@
+package com.filmrental.FilmRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filmrental/FilmRental/FilmRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmrental.FilmRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
