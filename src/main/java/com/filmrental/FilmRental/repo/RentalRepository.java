31436361f067bd075c5ba644e9c5610a0f38246a
Suraj Singh Rawal <surajrawal228@gmail.com>
2026-03-25 17:00:31 +0530

    Created Rental Entity with get api

---
+++ src/main/java/com/filmrental/FilmRental/repo/RentalRepository.java
@@ -0,0 +1,7 @@
+package com.filmrental.FilmRental.repo;
+
+import com.filmrental.FilmRental.model.Rental;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RentalRepository extends JpaRepository<Rental, Long> {
+}
---
+++ src/main/java/com/filmrental/FilmRental/service/RentalService.java
@@ -0,0 +1,8 @@
+package com.filmrental.FilmRental.service;
+
+import com.filmrental.FilmRental.model.Rental;
+import java.util.List;
+
+public interface RentalService {
+    List<Rental> getAllRentals();
+}
