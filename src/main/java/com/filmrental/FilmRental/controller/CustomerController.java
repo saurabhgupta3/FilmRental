4cf394263b888d5ccf43741b6820c30ce9196026
Aditya Singh <useradityasingh8@gmail.com>
2026-03-28 14:58:38 +0530

    worked on customer

--- src/main/java/com/filmrental/FilmRental/controller/CustomerController.java
+++ src/main/java/com/filmrental/FilmRental/controller/CustomerController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import com.filmrental.FilmRental.model.Customer;
 import com.filmrental.FilmRental.service.CustomerService;
-
+// REST controller to handle customer-related APIs: get customer by ID and fetch all customers
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
